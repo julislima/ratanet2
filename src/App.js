540cae5301710bce7a79485e9cb82bff22053d03
AMANDA POMPONET DOS SANTOS <021.898677@senaicimatec.edu.br>
2024-04-12 17:53:16 -0300

    primeiro commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css'; // Certifique-se de que o arquivo App.css existe
+import Nav from './menu.js';
+
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Nav />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componente/rata.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import  componente from 'react';
+import ratanet from './componente/ratanet.png';
+
+function App() {
+  return (
+
+    <div>
+       <img src={ratanet.png} alt="Minha Imagem" />
+    </div>
+
+
+        );
+}
+
+export default App;
